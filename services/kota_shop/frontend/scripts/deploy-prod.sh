b41760cc6b04618a99d179d39549b4a0c8c365d5
Samuel Masubelele <31527623+SamFSD@users.noreply.github.com>
2024-08-27 12:24:18 +0000

    update deploye script

--- services/kota_shop/frontend/scripts/deploy-prod.sh
+++ services/kota_shop/frontend/scripts/deploy-prod.sh
@@ -5,5 +5,3 @@ set -e
 cd services/kota_shop/frontend/app/
 
 yarn ng build --configuration production --aot
-# sudo cp -R ~/server/fleet_wb/services/kota_shop/frontend/app/dist/* :: TODO
-# firebase deploy
