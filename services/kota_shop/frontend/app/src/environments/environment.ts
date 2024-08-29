a3f06dc38b0b953dae53fedffa8f87b72b38e959
Samuel Masubelele <31527623+SamFSD@users.noreply.github.com>
2024-08-29 14:52:30 +0200

    Update environment.ts

--- services/kota_shop/frontend/app/src/environments/environment.ts
+++ services/kota_shop/frontend/app/src/environments/environment.ts
@@ -1,36 +1,12 @@
-// environment.ts
-
-// Common configuration
-const commonAuth = {
-  domain: 'dev-v0wwscx8d8hz7hj6.us.auth0.com',
-  clientId: 'o9vPpOTBqPkcDuC6oUAqDgljoBD1WY6P',
-  audience: 'https://dev-v0wwscx8d8hz7hj6.us.auth0.com/api/v2/'
-};
-
-// Environment-specific configuration
-const environmentConfig = {
-  production: {
-    production: true,
-    _API_AUTH_BASE_PATH: 'http://localhost:8080/api',
-    API_BASE_PATH: 'http://localhost:8080/api',
-    auth: {
-      ...commonAuth,
-      redirectUri: 'http://sam2awsbucket.s3-website.eu-north-1.amazonaws.com/'
-    }
-  },
-  local: {
-    production: false,
-    _API_AUTH_BASE_PATH: 'http://localhost:8080/api',
-    API_BASE_PATH: 'http://localhost:8080/api',
-    auth: {
-      ...commonAuth,
-      redirectUri: 'http://localhost:4200/'
-    }
+export const environment = {
+  production: false,
+  _API_AUTH_BASE_PATH: 'http://localhost:8080/api',
+  API_BASE_PATH: 'http://localhost:8080/api',
+  auth: {
+    domain: 'dev-v0wwscx8d8hz7hj6.us.auth0.com',
+    clientId: 'o9vPpOTBqPkcDuC6oUAqDgljoBD1WY6P',
+    redirectUri: 'http://localhost:4200/', /// for local
+    //redirectUri: 'http://localhost:4200/', /// for local
+    audience: 'https://dev-v0wwscx8d8hz7hj6.us.auth0.com/api/v2/'
   }
-};
-
-// Determine the current environment
-const isProduction = false; // Set this flag based on your build configuration or environment
-
-// Export the appropriate configuration
-export const environment = isProduction ? environmentConfig.production : environmentConfig.local;
+}
