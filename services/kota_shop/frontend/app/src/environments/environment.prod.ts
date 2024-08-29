80b68c551c0087eb7355b39bb25a19eadbfa2571
Samuel Masubelele <31527623+SamFSD@users.noreply.github.com>
2024-08-29 13:30:51 +0200

    Update environment.prod.ts

--- services/kota_shop/frontend/app/src/environments/environment.prod.ts
+++ services/kota_shop/frontend/app/src/environments/environment.prod.ts
@@ -1,12 +1,11 @@
 export const environment = {
-  production: true,
-  _API_AUTH_BASE_PATH: 'http://localhost:8000/*',
-  API_BASE_PATH: 'http://localhost:8000',
-
+  production: false,
+  _API_AUTH_BASE_PATH: 'http://localhost:8080/api',
+  API_BASE_PATH: 'http://localhost:8080/api',
   auth: {
     domain: 'dev-v0wwscx8d8hz7hj6.us.auth0.com',
     clientId: 'o9vPpOTBqPkcDuC6oUAqDgljoBD1WY6P',
-    redirectUri: 'http://sam2awsbucket.s3-website.eu-north-1.amazonaws.com/',
-    audience: 'https://dev-v0wwscx8d8hz7hj6.us.auth0.com/api/v2/' 
-  },
+    redirectUri: 'http://localhost:4200/',
+    audience: 'https://dev-v0wwscx8d8hz7hj6.us.auth0.com/api/v2/'
+  }
 };
